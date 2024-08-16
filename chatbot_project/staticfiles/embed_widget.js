217a3c01106bac7ec3f6c9430a38db9480fe42c5
Iulian Matei <iulianmatei19768@gmail.com>
2024-08-16 18:18:10 +0300

    Update embed_widget.js

--- chatbot_project/staticfiles/embed_widget.js
+++ chatbot_project/staticfiles/embed_widget.js
@@ -4,12 +4,22 @@
 
     const customText = document.currentScript.getAttribute('data-custom-text') || "Thank You";
 
+   
+
     fetch(`https://chatbotnewv3-production.up.railway.app/widget/?custom_text=${encodeURIComponent(customText)}`)
-        .then(response => response.text())
+        .then(response => {
+            console.log("Fetch response:", response);
+            return response.text();
+        })
         .then(html => {
+            console.log("Fetched HTML:", html);
             widgetContainer.innerHTML = html;
         })
         .catch(error => {
             console.error('Error loading widget content:', error);
         });
+
+    console.log("Script completed");
+
+    
 })();
