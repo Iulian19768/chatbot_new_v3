fe7deb59eeaeb18a524d962aac928ecfb1da6050
Iulian Matei <iulianmatei19768@gmail.com>
2024-08-16 21:57:44 +0300

    Update embed_widget.js

--- chatbot_project/test_app/static/embed_widget.js
+++ chatbot_project/test_app/static/embed_widget.js
@@ -6,9 +6,13 @@
     // Create an iframe to load content from your Django project
     var iframe = document.createElement('iframe');
     iframe.src = 'https://chatbotnewv3-production.up.railway.app/widget/?custom_text=' + encodeURIComponent(customText);
-    iframe.width = '100%';
-    iframe.height = '400px'; // Set appropriate height for your content
+    iframe.width = 'auto';
+    iframe.height = 'auto'; // Set appropriate height for your content
     iframe.style.border = 'none';
+    iframe.style.bottom = '4px';
+    iframe.style.right='4px';
+    iframe.style.position = 'fixed';
+
 
     // Append the iframe to the body (or any other container)
     document.body.appendChild(iframe);
