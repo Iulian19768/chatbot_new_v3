0091811c86fd5e45e032a5b0c8e6f425894f67e9
Iulian Matei <iulianmatei19768@gmail.com>
2024-08-16 15:27:05 +0300

    update

--- chatbot_project/test_app/static/embed_widget.js
+++ chatbot_project/test_app/static/embed_widget.js
@@ -4,7 +4,7 @@
 
     const customText = document.currentScript.getAttribute('data-custom-text') || "Thank You";
 
-    fetch(`https://chatbotnewv3-production.up.railway.app/widget/?custom_text=${encodeURIComponent(customText)}`)
+    fetch(`http://chatbotnewv3-production.up.railway.app/widget/?custom_text=${encodeURIComponent(customText)}`)
         .then(response => response.text())
         .then(html => {
             widgetContainer.innerHTML = html;
