5b7293eefd31cc246784917afc61e6e7661d0ef1
Iulian Matei <iulianmatei19768@gmail.com>
2024-08-16 21:32:47 +0300

    update

--- chatbot_project/test_app/middleware.py
+++ chatbot_project/test_app/middleware.py
@@ -2,5 +2,6 @@ from django.utils.deprecation import MiddlewareMixin
 
 class ContentSecurityPolicyMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        response['Content-Security-Policy'] = "frame-ancestors https://chatbotnewv3-production.up.railway.app"
+        # Update the CSP header to allow framing from specific domains
+        response['Content-Security-Policy'] = "frame-ancestors https://chatbotnewv3-production.up.railway.app/widget"
         return response
