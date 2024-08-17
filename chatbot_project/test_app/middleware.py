9fc4aa17b92acd49434b9bfc46a000980e519cda
Iulian Matei <iulianmatei19768@gmail.com>
2024-08-17 14:21:21 +0300

    update

--- chatbot_project/test_app/middleware.py
+++ chatbot_project/test_app/middleware.py
@@ -1,6 +1,21 @@
 from django.utils.deprecation import MiddlewareMixin
 
-class XFrameOptionsMiddleware(MiddlewareMixin):
+class CustomCookieMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        response['X-Frame-Options'] = 'ALLOW-FROM https://chatbotnewv3-production.up.railway.app/'
+        # Modify the session cookie
+        if 'sessionid' in request.COOKIES:
+            response.set_cookie(
+                'sessionid',
+                request.COOKIES['sessionid'],
+                samesite='None',
+                secure=True,
+            )
+        # Modify the CSRF cookie
+        if 'csrftoken' in request.COOKIES:
+            response.set_cookie(
+                'csrftoken',
+                request.COOKIES['csrftoken'],
+                samesite='None',
+                secure=True,
+            )
         return response
