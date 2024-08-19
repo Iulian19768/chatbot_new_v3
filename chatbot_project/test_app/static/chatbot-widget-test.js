e247e66e7f9a8f155bbc36b1027a3600fb5013a6
Iulian Matei <iulianmatei19768@gmail.com>
2024-08-19 09:46:54 +0300

    update

--- chatbot_project/test_app/static/chatbot-widget-test.js
+++ chatbot_project/test_app/static/chatbot-widget-test.js
@@ -3,24 +3,13 @@ window.initChatbot = function(options) {
     
     // Add button color as a query parameter to the URL
     var url = new URL(options.url || "https://chatbotnewv3-production.up.railway.app/");
-    
     if (options.buttonColor) {
-        // Apply color to the "Back" button
-        var backButton = document.getElementById('prewbtn');
-        if (backButton) {
-            backButton.style.backgroundColor = buttonColor;
-        }
-
-        // Apply color to the "Forward" button
-        var forwardButton = document.getElementById('nextbtn');
-        if (forwardButton) {
-            forwardButton.style.backgroundColor = buttonColor;
-        }
-        var chat_immages = document.getElementsByClassName('chat_immages');
-        for (var i = 0; i < chat_immages.length; i++) {
-            chat_immages[i].style.backgroundColor = buttonColor;
-        }
+        url.searchParams.set('buttonColor', options.buttonColor);
     }
+    if (options.classColor) {
+        url.searchParams.set('classColor', options.classColor);
+    }
+
 
     iframe.src = url.toString();
     iframe.style.position = 'fixed';
